028e02a010431dbb239a1f70ade691e8d7c9a8bf
KevinPhuc <kevinphuc.work@gmail.com>
2025-01-05 12:18:07 +0700

    App service & database

---
+++ app/src/main/java/com/mobileapp/habittracker/database/AppDatabase.kt
@@ -0,0 +1,37 @@
+package com.mobileapp.habittracker.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.mobileapp.habittracker.database.dao.HabitDao
+import com.mobileapp.habittracker.database.dao.TaskLogDao
+import com.mobileapp.habittracker.models.Habit
+import com.mobileapp.habittracker.models.TaskLog
+
+@Database(entities = [Habit::class, TaskLog::class], version = 1, exportSchema = true)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun habitDao() : HabitDao
+    abstract fun taskLogDao() : TaskLogDao
+
+    companion object {
+        private const val DATABASE_NAME = "database_app"
+        const val DATABASE_LOG_TAG = "database_log"
+
+        @Volatile
+        private var DATABASE_INSTANCE : AppDatabase? = null
+
+        fun getDatabase(context: Context) : AppDatabase {
+            return DATABASE_INSTANCE?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java, DATABASE_NAME)
+                    .build()
+
+                DATABASE_INSTANCE = instance
+
+                return@synchronized instance
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/mobileapp/habittracker/database/repositories/HabitRepository.kt
@@ -0,0 +1,60 @@
+package com.mobileapp.habittracker.database.repositories
+
+import android.util.Log
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import com.mobileapp.habittracker.database.AppDatabase
+import com.mobileapp.habittracker.database.dao.HabitDao
+import com.mobileapp.habittracker.models.Habit
+import com.mobileapp.habittracker.models.HabitWithTaskLogs
+
+@Suppress("RedundantSuspendModifier")
+
+class HabitRepository(private val habitDao: HabitDao) {
+    val habits = habitDao.getAll()
+    val habitsWithTaskLogs = habitDao.getHabitsWithTaskLogs()
+
+    @WorkerThread
+    suspend fun createHabit(habit : Habit) : Long {
+        val id = habitDao.create(habit)
+        Log.d(AppDatabase.DATABASE_LOG_TAG, "Habit inserted to database with id $id")
+
+        return id
+    }
+
+    @WorkerThread
+    suspend fun deleteHabit(habit : Habit) : Int {
+        val rows = habitDao.delete(habit)
+
+        Log.d(AppDatabase.DATABASE_LOG_TAG, "$rows rows deleted from the database")
+
+        return rows
+    }
+
+    @WorkerThread
+    suspend fun updateHabit(habit : Habit) : Int {
+        val rows = habitDao.update(habit)
+
+        Log.d(AppDatabase.DATABASE_LOG_TAG, "$rows rows updated in the database")
+
+        return rows
+    }
+
+    @WorkerThread
+    suspend fun getHabitById(id : Long) : Habit? {
+        return habitDao.getById(id)
+    }
+
+    fun getHabitWithTaskLogsById(id : Long) : LiveData<HabitWithTaskLogs?> {
+        return habitDao.getHabitWithTaskLogsById(id)
+    }
+
+    @WorkerThread
+    suspend fun deleteAll() : Int {
+        val rows = habitDao.deleteAll()
+
+        Log.d(AppDatabase.DATABASE_LOG_TAG, "$rows rows deleted from the database")
+
+        return rows
+    }
+}
