4007a1a383baad2d01030a11ed48c664a10e5f9a
HuynhVanVinh <103840023+HuynhVanVinh@users.noreply.github.com>
2025-01-05 08:51:41 +0700

    Edit part of application

---
+++ app/src/main/java/com/mobileapp/habittracker/database/repositories/TaskLogRepository.kt
@@ -0,0 +1,44 @@
+package com.mobileapp.habittracker.database.repositories
+
+import android.util.Log
+import androidx.annotation.WorkerThread
+import com.mobileapp.habittracker.database.AppDatabase
+import com.mobileapp.habittracker.database.dao.TaskLogDao
+import com.mobileapp.habittracker.models.TaskLog
+
+@Suppress("RedundantSuspendModifier")
+
+class TaskLogRepository(private val taskLogDao: TaskLogDao) {
+    val taskLogs = taskLogDao.getAll()
+
+    @WorkerThread
+    suspend fun createTaskLog(taskLog: TaskLog) : Long {
+        val id = taskLogDao.create(taskLog)
+        Log.d(AppDatabase.DATABASE_LOG_TAG, "TaskLog inserted to database with id $id")
+
+        return id
+    }
+
+    @WorkerThread
+    suspend fun deleteTaskLog(taskLog: TaskLog) : Int {
+        val rows = taskLogDao.delete(taskLog)
+
+        Log.d(AppDatabase.DATABASE_LOG_TAG, "$rows rows deleted from the database")
+
+        return rows
+    }
+
+    @WorkerThread
+    suspend fun updateTaskLog(taskLog: TaskLog) : Int {
+        val rows = taskLogDao.update(taskLog)
+
+        Log.d(AppDatabase.DATABASE_LOG_TAG, "$rows rows updated in the database")
+
+        return rows
+    }
+    
+    @WorkerThread
+    suspend fun getTaskById(id : Long) : TaskLog? {
+        return taskLogDao.getById(id)
+    }
+}
